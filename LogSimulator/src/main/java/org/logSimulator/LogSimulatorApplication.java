900e4f4c1da3d81abf00dc5d0e20b3cfb2b714f8
jeongsanghyeon <future9704@naver.com>
2023-10-09 19:27:30 +0900

    Added Scheduled $ added dependencies

--- LogSimulator/src/main/java/org/logSimulator/LogSimulatorApplication.java
+++ LogSimulator/src/main/java/org/logSimulator/LogSimulatorApplication.java
@@ -3,8 +3,10 @@ package org.logSimulator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @PropertySource(value = {"classpath:local/Application.properties"},
         ignoreResourceNotFound = true)
 public class LogSimulatorApplication {
--- LogSimulator/src/main/java/org/logSimulator/component/LoggerComponent.java
+++ LogSimulator/src/main/java/org/logSimulator/component/LoggerComponent.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 @Configuration
 public class LoggerComponent {
 
-    @Value("${test}")
+    @Value("${log.cycle}")
     String a;
 
     @Bean
