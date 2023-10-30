a5a61805cc4023b6396cfd24fad70bd309211fca
jeongsanghyeon <future9704@naver.com>
2023-10-31 01:29:54 +0900

    create topic maker

--- LogSimulator/src/main/java/com/simulator/logsimulator/configuration/KafkaConfig.java
+++ LogSimulator/src/main/java/com/simulator/logsimulator/configuration/KafkaConfig.java
@@ -1,5 +1,7 @@
 package com.simulator.logsimulator.configuration;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -15,9 +17,18 @@ public class KafkaConfig {
     public Producer<String, String> kafkaProducer() {
         // Kafka Producer 설정
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "your-kafka-server:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         return new KafkaProducer<>(props);
+    }
+    @Bean
+    public AdminClient topicMaker() {
+        // Kafka 브로커 및 관리자 클라이언트 설정
+        // 필요한지 확인해야함
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Kafka 브로커 서버 주소
+        properties.put(AdminClientConfig.CLIENT_ID_CONFIG, "CreateKafkaTopicExample");
+        return AdminClient.create(properties);
     }
 }
--- LogSimulator/src/main/java/com/simulator/logsimulator/service/KafkaService.java
+++ LogSimulator/src/main/java/com/simulator/logsimulator/service/KafkaService.java
@@ -1,18 +1,25 @@
 package com.simulator.logsimulator.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
 @Slf4j
 @Service
 public class KafkaService {
     private final Producer<String, String> kafkaProducer;
+    private final AdminClient topicMaker;
 
     @Autowired
-    public KafkaService(Producer<String, String> kafkaProducer) {
+    public KafkaService(Producer<String, String> kafkaProducer, AdminClient topicMaker) {
         this.kafkaProducer = kafkaProducer;
+        this.topicMaker = topicMaker;
     }
 
     public void sendMessage(String topic, String key, String value) {
@@ -24,5 +31,18 @@ public class KafkaService {
                 log.error("메시지 전송 실패: - {}", exception.getMessage());
             }
         });
+    }
+
+    public boolean createTopic(String topicName) {
+        // 토픽 이름, 파티션 수, 리플리케이션 팩터 설정
+        int numPartitions = 3;
+        short replicationFactor = 1; // 리플리케이션 팩터 (브로커 수에 따라 조정)
+
+        // 토픽 생성
+        NewTopic newTopic = new NewTopic(topicName, numPartitions, replicationFactor);
+        topicMaker.createTopics(Collections.singletonList(newTopic));
+
+        log.info("Create Topic - {}", topicName);
+        return true;
     }
 }
